89754218de1cc729e8b17125f5ce682794dd4143
ycdfwzy <ycdfwzy@outlook.com>
2019-11-28 17:40:53 +0800

    add synchronize

--- android/app/src/main/java/com/example/acoustic_motion_tracking_receiver/FMCW.java
+++ android/app/src/main/java/com/example/acoustic_motion_tracking_receiver/FMCW.java
@@ -1,20 +1,18 @@
 package com.example.acoustic_motion_tracking_receiver;
 
-import java.util.Arrays;
-
 public class FMCW {
-    public static int total = 88;
+    public static int total = 8;
     public static double fs = 44100;
     public static double T = 0.04;
     public static double f0 = 18000;
     public static double f1 = 20500;
+    public static int len = ((int)(fs*T)+1)*2;
     public static double[] chirp_data = SignalProcessingUtil.chirp_linear(fs, f0, T, f1);
     public static int FFTlen = 1024 * 16;  // reduce FFTlen if too slow
 
     public static double[] pseudo_T = null;
 
     public static double[] get_distance(double[] received_signal) {
-        int len = ((int)(fs*T)+1)*2;
         // pseudo-transmitted signal
         if (pseudo_T == null) {
             pseudo_T = new double[len*total];
@@ -58,13 +56,76 @@ public class FMCW {
         return delta_distance;
     }
 
+    public static int get_start(double[] received_signal) {
+        // pseudo-transmitted signal
+        if (pseudo_T == null) {
+            pseudo_T = new double[len*total];
+            for (int i = 0; i < total; i++) {
+                System.arraycopy(chirp_data, 0, pseudo_T, len*i, chirp_data.length);
+            }
+        }
+
+        // filtering
+        double[] filtered_signal = filtering(received_signal, 800, f0-1000, f1+1000, fs, "bp", "hamming");
+
+        // find start position
+        int start_idx = 0;
+        double[] c = SignalProcessingUtil.xcorr(filtered_signal, chirp_data);
+        double max_c = max(c);
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] > max_c/2) {
+                start_idx = i + 1 - filtered_signal.length;
+                break;
+            }
+        }
+        return start_idx;
+    }
+
+    public static double[] get_distance2(double[] received_signal) {
+        // pseudo-transmitted signal
+        if (pseudo_T == null) {
+            pseudo_T = new double[len*total];
+            for (int i = 0; i < total; i++) {
+                System.arraycopy(chirp_data, 0, pseudo_T, len*i, chirp_data.length);
+            }
+        }
+
+        if (received_signal.length != len*total) {
+            System.out.println("Error: get_distance2's received signal is not a period!");
+        }
+
+        // filtering
+        double[] filtered_signal = filtering(received_signal, 800, f0-1000, f1+1000, fs, "bp", "hamming");
+
+        // dot product
+        double[] s = new double[len*total];
+        for (int i = 0; i < s.length; i++)
+            s[i] = pseudo_T[i] * filtered_signal[i];
+
+        // fft to get distance
+        Complex[] tmp_s = new Complex[FFTlen];
+        for (int i = len/2; i < FFTlen; i++)
+            tmp_s[i] = new Complex();
+        double[] delta_distance = new double[total];
+        for (int i = 0; i < total; i++) {
+            for (int j = 0; j < len/2; j++)
+                tmp_s[j] = new Complex(s[i*len+j]);
+            Complex[] FFTout = SignalProcessingUtil.FFT(tmp_s);
+            int max_arg = maxarg(FFTout);
+            delta_distance[i] = (max_arg+1) * 340 * T / (f1-f0);
+        }
+
+        return delta_distance;
+
+    }
+
     public static double[] filtering(double[] raw_signal, int taps, double f1, double f2, double fs, String type, String window) {
         filter bp_filter = new filter(taps, f1/fs, f2/fs, "bp", "hamming");
         double[] h_bp = bp_filter.get_coeff();
 
         double[] bp_filtered_sig = new double[raw_signal.length];
         for(int i=0; i<raw_signal.length; i++) {
-            if (i%100==0) System.out.println(i);
+            // if (i%100==0) System.out.println(i);
             bp_filtered_sig[i] = bp_filter.filter(raw_signal[i]);
         }
         return bp_filtered_sig;
--- android/app/src/main/java/com/example/acoustic_motion_tracking_receiver/MainActivity.java
+++ android/app/src/main/java/com/example/acoustic_motion_tracking_receiver/MainActivity.java
@@ -19,10 +19,13 @@ import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.io.FileOutputStream;
+import java.util.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class MainActivity extends AppCompatActivity {
 
-    //48K采样率
+    //44.1K采样率
     int SamplingRate = 44100;
     //格式：单声道
     int channelConfiguration = AudioFormat.CHANNEL_IN_MONO;
@@ -30,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
     int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
     //是否在录制
     boolean isRecording = false;
+
+    BlockingQueue<Double> queue = new LinkedBlockingQueue<>();
+    Thread producer_thread, consumer_thread;
+    double[] acc_distance = null;
 
     int inputBufferSize = 0;
 
@@ -50,54 +57,87 @@ public class MainActivity extends AppCompatActivity {
 
         stop_button.setEnabled(false);
 
-        final int limit_len = 500000;
-        final double[] data_container = new double[limit_len];
+        // final int limit_len = 500000;
+        // final double[] data_container = new double[limit_len];
 
         start_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 start_button.setEnabled(false);
                 stop_button.setEnabled(true);
-                Thread thread = new Thread(new Runnable() {
+//                Thread thread = new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        int data_len = StartRecord(data_container, limit_len);
+//                        double[] received_data = new double[data_len];
+//                        System.arraycopy(data_container, 0, received_data, 0, data_len);
+//                        double[] delta_distance = FMCW.get_distance(received_data);
+//                        draw_line_graph(signal_view, delta_distance);
+//                    }
+//                });
+//                thread.start();
+                producer_thread = new Thread(new Runnable() {
                     @Override
                     public void run() {
-//                        double fs = 1000, f1 = 40/fs, f2 = 80/fs;
-//                        int m=800;
-//                        filter bp_filter = new filter(m, f1, f2, "bp", "hamming");
-//                        double[] h_bp = bp_filter.get_coeff();
-//                        Complex[] signal = new Complex[(int)fs*10];
-//                        double laps = 10/(10*fs-1), t, f1_demo = 10, f2_demo = 75, f3_demo = 150;
-//
-//                        for(int i=0; i<signal.length; i++)
-//                        {
-//                            t = i*laps;
-//                            signal[i] =new Complex(Math.sin(2*Math.PI*f1_demo*t)+Math.sin(2*Math.PI*f2_demo*t)+Math.sin(2*Math.PI*f3_demo*t));
-//                        }
-//                        Complex[] bp_filtered_sig = new Complex[(int)fs*10];
-//                        double[] xs = new double[signal.length];
-//                        for(int i=0; i<signal.length; i++)
-//                        {
-//                            bp_filtered_sig[i] = new Complex(bp_filter.filter(signal[i].real()));
-//                            xs[i] = i*(fs/(signal.length-1));
-//                        }
-//                        Complex[] fft_sig = SignalProcessingUtil.FFT(signal), filtered_fft = SignalProcessingUtil.FFT(bp_filtered_sig);
-//                        double[] ys = new double[signal.length];
-//                        for(int i=0; i<signal.length; i++)
-//                        {
-//                            ys[i] = filtered_fft[i].abs();
-//                        }
-//                        draw_line_graph(signal_view, xs, ys);
-                        int data_len = StartRecord(data_container, limit_len);
-                        double[] received_data = new double[data_len];
-                        for(int i=0; i<data_len; i++)
-                        {
-                            received_data[i] = data_container[i];
-                        }
-                        double[] delta_distance = FMCW.get_distance(received_data);
-                        draw_line_graph(signal_view, delta_distance);
+                        // record data
+                        StartRecord();
                     }
                 });
-                thread.start();
+                consumer_thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // analyze data
+                        Queue<Double> buffer = new LinkedList<>();
+                        try {
+                            // get start
+                            for (int i = 0; (i < FMCW.total * FMCW.len * 3) && isRecording; i++) {
+                                if (!queue.isEmpty()) {
+                                    buffer.add(queue.take());
+                                } else
+                                    i--;
+                            }
+                            if (!isRecording) return;
+                            Object[] tmp = buffer.toArray();
+                            double[] pre_data = new double[FMCW.total * FMCW.len * 3];
+                            for (int i = 0; i < FMCW.total * FMCW.len * 3; i++)
+                                pre_data[i] = (double) tmp[i];
+                            int start_idx = FMCW.get_start(pre_data);
+                            for (int i = 0; i < start_idx; i++) {
+                                buffer.poll();
+                            }
+
+                            // analyze
+                            double[] received_data = new double[FMCW.total * FMCW.len];
+                            while (isRecording) {
+                                while (isRecording && buffer.size() < FMCW.total * FMCW.len) {
+                                    buffer.add(queue.take());
+                                }
+                                if (!isRecording) return;
+
+                                for (int i = 0; i < FMCW.total * FMCW.len; i++) {
+                                received_data[i] = buffer.poll();
+                            }
+                            double[] delta_distance = FMCW.get_distance2(received_data);
+                            if (acc_distance == null) {
+                                acc_distance = delta_distance;
+                            } else
+                            {
+                                double[] temp = acc_distance;
+                                acc_distance = new double[temp.length + delta_distance.length];
+                                System.arraycopy(temp, 0, acc_distance, 0, temp.length);
+                                System.arraycopy(delta_distance, 0, acc_distance, temp.length, delta_distance.length);
+                            }
+                            draw_line_graph(signal_view, acc_distance);
+                        }
+
+                        } catch (InterruptedException e) {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                });
+                isRecording = true;
+                producer_thread.start();
+                consumer_thread.start();
             }
         });
 
@@ -107,6 +147,8 @@ public class MainActivity extends AppCompatActivity {
                 isRecording = false;
                 start_button.setEnabled(true);
                 stop_button.setEnabled(false);
+                producer_thread.interrupt();
+                // consumer_thread.interrupt();
             }
         });
     }
@@ -174,51 +216,33 @@ public class MainActivity extends AppCompatActivity {
         line_view.getViewport().setMaxY(max_y);
     }
 
-    public int StartRecord(double[] data_container, int limit_len)
-    {
+    public void StartRecord(/*double[] data_container, int limit_len*/) {
         inputBufferSize = AudioRecord.getMinBufferSize(SamplingRate, channelConfiguration, audioEncoding);
         AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, SamplingRate, channelConfiguration, audioEncoding, inputBufferSize);
         int size_short = inputBufferSize/2, size_float = inputBufferSize/4;
         short[] buffer = new short[size_short];
-        int start_pos = 0;
+//        int start_pos = 0;
 
-        try{
-//            FileOutputStream out = new FileOutputStream(Environment.getExternalStorageDirectory().getAbsolutePath()+"/record.wav");
-
-            isRecording = true;
+        try {
             audioRecord.startRecording();
             while (isRecording) {
-//                int bufferReadResult = audioRecord.read(buffer, 0, inputBufferSize);
-
                 int bufferReadResult = audioRecord.read(buffer, 0, size_short);
-                for(int i=0; i<bufferReadResult; i++)
-                {
-                    data_container[start_pos+i] = (double) buffer[i];
+                for (int i = 0; i < bufferReadResult; i++) {
+//                    data_container[start_pos+i] = (double) buffer[i];
+                    queue.put((double) buffer[i]);
                 }
-                start_pos+=bufferReadResult;
-                if(start_pos+bufferReadResult>=limit_len)
-                    isRecording = false;
+//                start_pos+=bufferReadResult;
+//                if(start_pos+bufferReadResult>=limit_len)
+//                    isRecording = false;
             }
-//            byte[] to_write = new byte[start_pos*2];
-//            for(int i=0; i<start_pos; i++)
-//            {
-//                to_write[2*i] = (byte)(data_container[i]&0xff);
-//                to_write[2*i+1] = (byte)((data_container[i]>>8)&0xff);
-//            }
-//
-//            long audio_len = start_pos*2;
-//
-//            long data_len = audio_len + 36;
-//            //为wav文件写文件头
-//            WriteWaveFileHeader(out, audio_len, data_len, (long)SamplingRate, 1, (long)(16 * SamplingRate * 1 / 8));
-//            out.write(to_write);
-//            out.close();
             audioRecord.stop();
+        } catch (InterruptedException e) {
+
         }
         catch (Throwable t) {
             Log.e("MainActivity", "录音失败");
         }
 
-        return start_pos;
+//        return start_pos;
     }
 }
